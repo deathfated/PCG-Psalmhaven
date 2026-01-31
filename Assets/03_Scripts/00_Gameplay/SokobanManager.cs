4fa39a9945394e02a5901f8652cc4ec0a09b10f3
Ardi-Hilal <ardi.itsna2@gmail.com>
2026-02-01 06:34:07 +0700

    Add Puzzle on Abyss 1

--- Assets/03_Scripts/00_Gameplay/SokobanManager.cs
+++ Assets/03_Scripts/00_Gameplay/SokobanManager.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class SokobanManager : MonoBehaviour
 {
     public TargetTrigger[] allTargets;
+    [SerializeField] private Animator Animasi;
 
     
     public void Start ()
     {
         
+        if (Animasi == null)
+            Animasi = GetComponent<Animator>();
+
+
         foreach(TargetTrigger trigger in allTargets)
         {
             trigger.initial(this);
@@ -32,11 +37,16 @@ public class SokobanManager : MonoBehaviour
     {
         
         Debug.Log("SEMUA BOX SUDAH MASUK!");
+    
+        Animasi.SetTrigger("OpenDoor");
 
         if (SoundManager.Instance != null && SoundManager.Instance.CompleteClip != null)
-    {
+        {
         SoundManager.Instance.PlaySFX(SoundManager.Instance.CompleteClip);
-    }
+        }
+
+        
+        
     }
 
 
